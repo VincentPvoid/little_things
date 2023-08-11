47a861d2e6261f31275c02b62c58b7e14611e33b
VincentPvoid <60776308+VincentPvoid@users.noreply.github.com>
2023-08-11 10:47:44 +0800

    add tumblr_ban_rader

---
+++ tumblr_ban_rader.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         tumblr屏蔽rader推荐
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  tumblr屏蔽rader推荐部分
+// @author       You
+// @match        https://www.tumblr.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  let timer = setInterval(() => {
+    let divs = document.getElementsByClassName('FZkjV');
+    divs = [].slice.call(divs);
+    console.log(divs, 'ddddddddddd')
+    if (divs.length > 1) {
+      // divs[1].style.display = "none";
+      divs[1].parentElement.removeChild(divs[1]);
+      clearInterval(timer)
+    }
+  }, 1000);
+})();
